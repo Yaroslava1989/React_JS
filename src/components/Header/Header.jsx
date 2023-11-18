c8e0dd0b703871c3c480d2b400e350fcd49cc7ea
Yaroslava1989 <blue-tokyo@ukr.net>
2023-11-18 18:46:13 +0200

    22 Route exact

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,16 @@ import s from './Header.module.css';
 const Header = () => {
     return (
         <header className={s.header}>
-            <img src='https://l450v.alamy.com/450v/2bra807/letter-vt-logo-icon-design-template-elements-2bra807.jpg' />
+            <div className={s.titleBlock}>
+                <div className={s.titleImg}>
+                  <img src='https://l450v.alamy.com/450v/2bra807/letter-vt-logo-icon-design-template-elements-2bra807.jpg' />
+                </div>
+                <div className={s.logoTitle}>
+                  <h6>Kamasutra-JS</h6>
+                  <h6>ASSBOOK</h6>
+                  <h6>SOSIAL NETWORK</h6>
+                </div>
+            </div>
         </header>
     );
 }
--- src/components/Navbar/Dialogs/Dialogs.jsx
+++ src/components/Navbar/Dialogs/Dialogs.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
 
 
 const Dialogs = (props) => {
-    return ( <div>
-        Dialogs
-        </div>
+    return ( <div className={s.dialogs}>
+              <div className={s.dialogsItems}>
+                  <div className={s.dialog +' '+ s.active}>
+                      <NavLink to="/dialogs/1">Yaroslava</NavLink>
+                  </div>
+                  <div className={s.dialog}>
+                      <NavLink to="/dialogs/2">Baby Mochi</NavLink>
+                  </div>
+                  <div className={s.dialog}>
+                      <NavLink to="/dialogs/3">Banny</NavLink>
+                  </div>
+                  <div className={s.dialog}>
+                      <NavLink to="/dialogs/4">WWH</NavLink>
+                  </div>
+                  <div className={s.dialog}>
+                      <NavLink to="/dialogs/5">The Sun</NavLink>
+                  </div>
+                  <div className={s.dialog}>
+                      <NavLink to="/dialogs/6">Best Leader</NavLink>
+                  </div>
+              </div>
+              <div className={s.messages}>
+                  <div className={s.message}>Hi!</div>
+                  <div className={s.message}>How is youre day?</div>
+                  <div className={s.message}>How is youre it-kamasutra?</div>
+              </div>
+              </div>
             );
 }
 
